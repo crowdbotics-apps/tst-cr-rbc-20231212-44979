f195d8b1bdffe3367b0aeb64c5f89872cb979d3e
Crowdbotics <team@crowdbotics.com>
2023-12-12 20:16:11 +0000

    Initial commit

---
+++ backend/home/migrations/0001_load_initial_data.py
@@ -0,0 +1,24 @@
+from django.db import migrations
+
+
+def create_site(apps, schema_editor):
+    Site = apps.get_model("sites", "Site")
+    custom_domain = "tst-cr-rbc-20231212-44979.botics.co"
+
+    site_params = {
+        "name": "TST-CR-RBC-20231212-02",
+    }
+    if custom_domain:
+        site_params["domain"] = custom_domain
+
+    Site.objects.update_or_create(defaults=site_params, id=1)
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("sites", "0002_alter_domain_unique"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_site),
+    ]
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tst-cr-rbc-20231212-44979",
+  url: "https://tst-cr-rbc-20231212-44979.botics.co",
+  api: "https://tst-cr-rbc-20231212-44979.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
